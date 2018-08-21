bf7049cf83a0f3f4426fe0f1caf88f3686ed2911
Julian Gründner <juliangruendner@googlemail.com>
2018-08-21 13:52:18 +0200

    add start option for docker env

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #python3 ds_queue.py -a 127.0.0.1 -p 8001 -r localhost:8843 -d proxyLog.logs -v -s
-python3 ds_queue.py -a 127.0.0.1 -p 8001 -r localhost:8843 -d proxyLog.logs -v -i -s
+#python3 ds_queue.py -a 127.0.0.1 -p 8001 -r localhost:8843 -d proxyLog.logs -v -i -s
 #python3 ds_queue.py -a 127.0.0.1 -p 8001 -r localhost:8843 -d proxyLog.logs -i -s
 #python ds_queue.py -a 127.0.0.1 -p 8001 -r localhost:8880 -d proxyLog.logs -v -x plugins/changeagent.py
+
+# start in docker container
+python3 /root/ds_queue/ds_queue.py -a 0.0.0.0 -p 8001 -r datashield_opal:8443 -d proxyLog.logs -v -i -s 
