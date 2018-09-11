a1a0c56e714fe2c387d46566eea2f4369b34c511
Julian Gründner <juliangruendner@googlemail.com>
2018-09-11 09:54:32 +0200

    add queue start with timeout to start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #python3 ds_queue.py -a 127.0.0.1 -p 8001 -r localhost:8843 -d proxyLog.logs -v -s
-python3 ds_queue.py -a 127.0.0.1 -p 8001 -r localhost:8843 -d proxyLog.logs -v -i -s
+#python3 ds_queue.py -a 127.0.0.1 -p 8001 -r localhost:8843 -d proxyLog.logs -v -i -s
+python3 ds_queue.py -a 127.0.0.1 -p 8001 -r localhost:8843 -d proxyLog.logs -v -i -s -t 10:10
 #python3 ds_queue.py -a 127.0.0.1 -p 8001 -r localhost:8843 -d proxyLog.logs -i -s
 #python ds_queue.py -a 127.0.0.1 -p 8001 -r localhost:8880 -d proxyLog.logs -v -x plugins/changeagent.py
 
